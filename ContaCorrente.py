0dc71b05276cc3fb0098e11cfddd8796ddca7c54
Guilherme Bezerra <guilhermebfneto@gmail.com>
2023-03-27 11:54:51 -0300

    Adição de guarda referente à impossibilidade de sacar ou depositar valores negativos nos métodos depositar() e sacar() da classe ContaCorrente

--- ContaCorrente.py
+++ ContaCorrente.py
@@ -8,9 +8,15 @@ class ContaCorrente(Conta):
         self.limite = limite
 
     def depositar(self, valor: float) -> None:
+        if valor < 0:
+            raise ValueError("Não é permitido depositar valores negativos")
+
         self.saldo += valor
 
     def sacar(self, valor: float) -> None:
+        if valor < 0:
+            raise ValueError("Não é permitido sacar valores negativos")
+        
         if self.saldo + self.limite < valor:
             raise ValueError("Valor maior que o permitido")
 
